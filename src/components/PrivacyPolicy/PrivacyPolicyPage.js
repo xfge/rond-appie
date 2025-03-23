ed2b11d275896259ab8ec99836a5a69926343bdb
Xiaofei GE <xfge.cn@gmail.com>
2025-03-23 16:40:14 +0800

    Update PrivacyPolicyPage.js

--- src/components/PrivacyPolicy/PrivacyPolicyPage.js
+++ src/components/PrivacyPolicy/PrivacyPolicyPage.js
@@ -146,15 +146,15 @@ function PrivacyPolicyPage() {
                                     <h4>Children’s Privacy</h4>
                                     <div>
                                         <p>
-                                            These Services do not address anyone under the age of 13.
-                                            I do not knowingly collect personally identifiable
+                                            These Services do not address anyone under the age of
+                                            13. I do not knowingly collect personally identifiable
                                             information from children under 13 years of age. In the
-                                            case I discover that a child under 13 has provided me with
-                                            personal information, I immediately delete this from our
-                                            servers. If you are a parent or guardian and you are aware
-                                            that your child has provided us with personal information,
-                                            please contact me so that I will be able to do the
-                                            necessary actions.
+                                            case I discover that a child under 13 has provided me
+                                            with personal information, I immediately delete this
+                                            from our servers. If you are a parent or guardian and
+                                            you are aware that your child has provided us with
+                                            personal information, please contact me so that I will
+                                            be able to do the necessary actions.
                                         </p>
                                     </div>
                                     <h4>Changes to This Privacy Policy</h4>
